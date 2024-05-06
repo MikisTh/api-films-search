dc7b81c2bc6d6cda525e532f1aa38ad5ca2df23e
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-05-06 10:04:55 -0300

    Update logo.tsx
    Add

--- components/logo.tsx
+++ components/logo.tsx
@@ -7,7 +7,7 @@ export function Xmdb() {
       href="/"
       className="inline-flex items-center justify-center h-10 gap-1 px-2 text-white bg-pink-600 rounded-lg hover:scale-110 transition-transform"
     >
-      <h1 className="sr-only">Xata Movie Database</h1>
+      <h1 className="sr-only">Mikis Films Database</h1>
       <MovieRoll aria-hidden className="text-2xl" />
       <span>XMDB</span>
     </Link>
